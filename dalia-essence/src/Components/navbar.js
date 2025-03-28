bdd4e13f700663aaa2bffa65cf1356caee57436f
Eleonora <Eleonorarusso592@gmail.com>
2025-03-28 11:35:25 +0100

    Refactor Navbar component to include user authentication and routing

--- dalia-essence/src/Components/navbar.js
+++ dalia-essence/src/Components/navbar.js
@@ -1,34 +1,38 @@
-import * as React, { useState } from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
+import React from 'react';
+import { useAuth } from '../Context/AuthContext';
+import { AppBar, Toolbar, Typography, Button, Box } from '@mui/material';
+import { Link } from 'react-router-dom';
 
-
+const Navbar = () => {
+  const { user, logout } = useAuth();
 
 export default function Navbar() {
   return (
-    <Box sx={{ flexGrow: 1 }}>
-        <AppBar position="static">
-            <Toolbar>
-            <IconButton
-                size="large"
-                edge="start"
-                color="#fff"
-                aria-label="menu"
-                sx={{ mr: 2 }}
-            >
-                <MenuIcon />
-            </IconButton>
-            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                Dalia Essence
-            </Typography>
-            <Button color="#fff">Login</Button>
-            </Toolbar>
-        </AppBar>
-    </Box>
-  ); 
-}
+    <AppBar position="static" color="primary">
+      <Toolbar>
+        {/* Titolo */}
+        <Typography variant="h6" sx={{ flexGrow: 1 }}>
+          <Link to="/" style={{ textDecoration: 'none', color: 'white' }}>Social Helper</Link>
+        </Typography>
+
+        {/* Mostra Login o Profilo/Logout */}
+        {user ? (
+          <>
+            <Button color="inherit" component={Link} to="/profile">
+              Profilo ({user.username})
+            </Button>
+            <Button color="inherit" onClick={logout}>
+              Logout
+            </Button>
+          </>
+        ) : (
+          <Button color="inherit" component={Link} to="/login">
+            Login
+          </Button>
+        )}
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Navbar;
