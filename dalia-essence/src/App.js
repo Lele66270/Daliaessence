97461a1eb4396d8d2bd1ffd09386933a3955e720
Eleonora <Eleonorarusso592@gmail.com>
2025-03-28 08:37:41 +0100

    Add Navbar component and integrate Bootstrap for styling

--- dalia-essence/src/App.js
+++ dalia-essence/src/App.js
@@ -5,10 +5,12 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
+import Navbar from './Components/navbar';
 
 function App() {
   return (
     <div>
+      <Navbar />
       <PostList />
     </div>
   );
